d64d70bf7a376f14a10f37fc5f02f1ab0d293df5
Korunk <korunka91@gmail.com>
2022-02-22 18:20:32 +0100

    1 file modified

--- promenne/ukol1.js
+++ promenne/ukol1.js
@@ -1,1 +1,15 @@
-console.log('ahoj');
+// Založte proměnné pro jméno, věk, váhu v kg a  výšku  v metrech - nastavte jim libovolnou hodnotu.
+
+let jméno = 'Klára';
+let věk = 30;
+let váha = 56;
+let výška = 168;
+
+ let BMI = (váha / výška ** 2);
+
+
+
+console.log ("Jmenuji se " + jméno ", je mi " + věk " let, vážím " + váha "kg,  měřím " výška "m a moje BMI je "BMI".'
+  );
+
+
