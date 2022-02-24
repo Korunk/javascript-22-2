45788b013e95e5f0df0a8afe4ae43f492dd732a9
Korunk <korunka91@gmail.com>
2022-02-24 20:06:22 +0100

    1 file modified

--- vstupy/3.js
+++ vstupy/3.js
@@ -1,12 +1,29 @@
 
 
-function jmenoFunkce (a, b) {
+//function jmenoFunkce (a, b) {
 
-  console.log (a + b);
-  console.log (Math.abs(a - b));
-  console.log (a * b);
-  console.log (a / b);
-}
+  //console.log (a + b);
+  //console.log (Math.abs(a - b));
+  //console.log (a * b);
+  //console.log (a / b);
+//}
 
 
-jmenoFunkce (5, 7);
+//jmenoFunkce (5, 7);
+
+//function prvocislo (a) {
+  //let prvocilo = true;
+  //for ( let i = 2; i < a; i++ )
+  //if (a % i == 0);
+  //rvocislo = false;
+  //break;
+//}
+
+//{
+  //return prvocislo;
+//}
+//let cislo = Number (prompt("Zadej číslo"));
+
+//if (prvocislo(cislo))
+
+
